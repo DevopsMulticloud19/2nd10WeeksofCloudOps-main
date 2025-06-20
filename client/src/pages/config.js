fb978402c5d7621884d022c91c395d0adb8205f5
DevopsMulticloud19 <cdevops49@gmail.com>
2025-06-20 18:46:45 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
- const API_BASE_URL = "http://3.95.200.67:84";
-//const API_BASE_URL = "http://3.95.200.67:84";
+// const API_BASE_URL = "http://3.95.200.67:84";
+const API_BASE_URL = "http://3.95.200.67:84";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
